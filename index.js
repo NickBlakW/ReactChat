036fc842e850099d40cc0e25d87ccf3e47cb285c
Nicolai Blak Walther <nicolai.blak@gmail.com>
2024-06-06 21:58:02 +0200

    push functionality
    Only works from firebase console

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import { PermissionsAndroid } from 'react-native';
+
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+// Register background handle
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
